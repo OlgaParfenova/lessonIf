44200a6d1e239c46e363d58167b6787cc2fde763
Olga Parfenova <work.parfenova@gmail.com>
2023-02-20 21:36:33 +0300

    The first module is completed

---
+++ script.js
@@ -0,0 +1,99 @@
+// task 1
+
+// console.log(false == 0); // ------ true
+// console.log(false == null); // false (falsy)
+// console.log(false == -0); // ------ true
+// console.log(false == NaN); // false (falsy)
+// console.log(false == undefined); // false (falsy)
+// console.log(false == ''); // ------ true
+
+// task 2
+
+// let test = 1;
+//
+// if (test == true) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 3
+
+// let test = 0;
+//
+// if (test == true) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 4
+
+// let test = '';
+//
+// if (test == false) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 5
+
+// let test;
+//
+// if (test == true) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 6
+
+// let test = 3 * 'a';
+//
+// if (test == true) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 7
+
+// let test = true;
+//
+// if (test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 8
+//
+// let test = true;
+//
+// if (!test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 9
+
+// let test = true;
+//
+// if (!test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// task 10
+
+// let test = true;
+//
+// if (test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
